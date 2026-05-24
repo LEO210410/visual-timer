3da4efb710998360ce665e11cc59220d028b7f42
LEO210410 <124023828+LEO210410@users.noreply.github.com>
2026-05-24 20:07:56 +0900

    Clean up comments in sw.js
    Remove comments in Japanese from service worker.

--- sw.js
+++ sw.js
@@ -7,14 +7,12 @@ const ASSETS = [
   './icon-512.png'
 ];
 
-// インストール時にファイルを保存
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// オフライン時はキャッシュから読み込む
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
